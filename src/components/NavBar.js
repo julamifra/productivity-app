5656c92ccd9d3b2aee9d547ee7d0767809e855c1
julamifra <julamifra@gmail.com>
2023-02-18 16:45:57 +0000

    create different routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="lg">
       <Container>
-        <Navbar.Brand href="#home"><img src={logo} alt="logo" height="35"/></Navbar.Brand>
+        <NavLink to="/" >
+          <Navbar.Brand href="#home"><img src={logo} alt="logo" height="35"/></Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="text-left">
@@ -21,12 +24,15 @@ const NavBar = () => {
             </NavDropdown>
           </Nav>
           <Nav className="ml-auto text-left">
-            <Nav.Link href="#signin">
+            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/" >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
               <i className="fas fa-sign-in-alt"></i>Sign in
-            </Nav.Link>
-            <Nav.Link href="#signup">
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active}  to="/signup">
               <i className="fas fa-user-plus"></i>Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
